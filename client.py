71c82497583f5047e143fb6eb67e0418546bb597
Ruchitha <ruchitharoyal07@gmail.com>
2026-05-15 04:14:29 +0530

    Initial commit - Chat application using socket programming

---
+++ client.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+host = '127.0.0.1'
+port = 5555
+
+nickname = input("Enter your name: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+# Receive messages
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+
+            if message == "NICK":
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("Error!")
+            client.close()
+            break
+
+# Send messages
+def write():
+    while True:
+        message = input("")
+        full_msg = f"{nickname}: {message}"
+        client.send(full_msg.encode('utf-8'))
+
+threading.Thread(target=receive).start()
+threading.Thread(target=write).start()
